79c84d99cb29f5f2b0b191a5b8c868b523a5971f
sneha-gowda <snehagowda9899@gmail.com>
2021-04-22 18:47:00 +0530

    created login and register Activity and designed xml files`

--- app/src/main/java/e/kailina/bukley/Main2Activity.java
+++ app/src/main/java/e/kailina/bukley/Main2Activity.java
@@ -2,11 +2,15 @@ package e.kailina.bukley;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
+    Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +23,30 @@ public class Main2Activity extends AppCompatActivity {
         MenuInflater menuInflater=getMenuInflater();
         menuInflater.inflate(R.menu.mainmenu,menu);
         return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Handle item selection
+        switch (item.getItemId()) {
+            case R.id.login:
+                Intent loginActivity=new Intent(Main2Activity.this,Login.class);
+                startActivity(loginActivity);
+                return true;
+            case R.id.getInTouch:
+                //perform any action;
+                return true;
+            case R.id.sellBook:
+                //perform any action;
+                return true;
+            case R.id.Register:
+                Intent registerActivity=new Intent(Main2Activity.this,Register.class);
+                startActivity(registerActivity);
+                return true;
+            case R.id.Logout:
+                //perform any action;
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
